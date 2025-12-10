b1b5d48b5fd07d030f2478bd193e7147feb6c2dd
Rajesh Jadoun <deo01@ntwoods.com>
2025-12-11 00:10:56 +0530

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@
  ***************************************************/
 
 // ===== CONFIG =====
-const API_BASE_URL = 'https://script.google.com/macros/s/AKfycbxh0etYDCdS7JmluGDhIjJT5JdI6GAtE0UyfTjIDon4OBXbryr5FIN73KCZYhW74jc_Zg/exec';
+const API_BASE_URL = 'https://script.google.com/macros/s/AKfycbxUXXUfHQ132Hy4Fp4cdwCzrAK_dr_MeHCsSjSPLlLMU51r35j2Op64aXImM9nWfEX3FQ/exec';
 const GOOGLE_CLIENT_ID = '1029752642188-ku0k9krbdbsttj9br238glq8h4k5loj3.apps.googleusercontent.com';
 
 // Walk-in form page
